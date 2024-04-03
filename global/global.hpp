23d8ebc38f15c8e220d9818ee26959dbb040ec15
OKOKDragon <141321330+OKOKDragon@users.noreply.github.com>
2024-04-03 10:16:13 +0800

    Update global.hpp

--- global/global.hpp
+++ global/global.hpp
@@ -1,10 +1,9 @@
-﻿#pragma once
+#pragma once
 
 /*
 此静态库的导出头文件
 包含此头文件，就能使用此库的所有功能
 */
-#include ".\cpp_std_lib.hpp"
 #include ".\string.hpp"
 #include ".\table_driver.hpp"
 #include ".\tree.hpp"
@@ -13,3 +12,4 @@
 #include ".\locker.hpp"
 #include ".\bits.hpp"
 #include ".\safety.hpp"
+#include ".\timer.hpp"
