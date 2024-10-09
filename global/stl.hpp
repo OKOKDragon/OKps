f5a106c220f1feef3723e28d52e611d9db7a7d55
OKOKDragon <141321330+OKOKDragon@users.noreply.github.com>
2024-10-09 19:58:44 +0800

    Add files via upload

---
+++ global/stl.hpp
@@ -0,0 +1,171 @@
+﻿#pragma once
+
+//导入标准库
+
+//c
+//工具库
+#include <csetjmp>//保存及跳转到执行环境的宏和函数
+#include <csignal>//信号管理
+#include <cstdarg>//变长实参列表
+#include <cstddef>//c标准宏、typedef
+#include <cstdlib>//c通用工具，包含内存分配、随机数、排序、搜索
+#include <ctime>//c时间工具
+//数值和极限
+#include <cfloat>//浮点类型
+#include <cinttypes>//格式化宏，数学与转换
+#include <climits>//整数类型的极限
+#include <cstdint>//定宽整数类型与其他类型的极限
+//错误处理
+#include <cassert>//条件编译宏
+#include <cerrno>//含有最后一次错误号的宏
+//字符串
+#include <cctype>//确定窄字符类别的函数
+#include <cstring>//窄字符串处理函数
+#include <cuchar>//unicode字符转换函数
+#include <cwchar>//宽字符串、多字节字符串处理函数
+#include <cwctype>//确定宽字符类别的函数
+//数学库
+#include <cfenv>//浮点环境访问函数
+#include <cmath>//数学函数
+//本地化
+#include <clocale>
+//输入输出
+#include <cstdio>
+
+//c++11及更低版本
+//工具库
+#include <bitset>
+#include <chrono>//c++时间工具
+#include <functional>
+#include <initializer_list>
+#include <tuple>
+#include <type_traits>//编译时类型信息
+#include <typeindex>
+#include <typeinfo>//运行是类型信息
+#include <utility>
+//动态内存管理
+#include <memory>
+#include <new>
+#include <scoped_allocator>//嵌套分配器
+//数值极限
+#include <limits>
+//错误处理
+#include <exception>
+#include <stdexcept>//标准异常对象
+#include <system_error>//依赖平台的错误码
+//字符串
+#include <string>
+//容器
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+//迭代器
+#include <iterator>
+//算法
+#include <algorithm>
+//数值
+#include <complex>
+#include <numeric>
+#include <random>
+#include <ratio>//编译期有理数算术
+#include <valarray>
+//本地化
+#include <locale>
+/*
+unicode转换
+在c++17弃用，在c++26移除
+*/
+#include <codecvt>
+//输入输出
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+//正则表达式
+#include <regex>
+//原子操作
+#include <atomic>
+//线程
+#include <condition_variable>//线程等待条件
+#include <future>//异步计算元件
+#include <mutex>
+#include <thread>
+
+//c++14
+//线程
+#include <shared_mutex>
+
+//c++17
+//工具库
+#include <any>
+#include <optional>
+#include <variant>
+//动态内存管理
+#include <memory_resource>//多态分配器和内存资源
+//字符串
+#include <charconv>
+#include <string_view>
+//算法
+#include <execution>//并行版本算法的预定义执行策略
+//文件系统
+#include <filesystem>
+
+//c++20
+//概念库
+#include <concepts>
+//协程库
+#include <coroutine>
+//工具库
+#include <compare>//三路运算符
+#include <source_location>//获取源码位置
+#include <version>//提供依赖实现的库信息
+//字符串
+#include <format>//格式化
+//容器
+#include <span>
+//范围
+#include <ranges>
+//数值
+#include <bit>//位操作
+#include <numbers>//数学常数
+//输入输出
+#include <syncstream>
+//线程
+#include <latch>
+#include <semaphore>//信号量
+#include <stop_token>
+
+//c++23
+//工具库
+//#include <expected>
+//数值极限
+//#include <stdfloat>//拓展浮点类型
+//异常处理
+//#include <stacktrace>//栈追踪
+//容器
+//#include <flat_map>
+//#include <flat_set>
+//#include <mdspan>
+//范围
+//#include <generator>
+//输入输出
+//#include <print>
+//#include <spanstream>
+
+//c++26
+//工具库
+//#include <debugging>
+//数值
+//#include <linalg>//基本线性代数算法
+//本地化
+//#include <text_encoding>
+//线程
+//#include <hazard_pointer>//风险指针
---
+++ global/string.hpp
@@ -0,0 +1,118 @@
+﻿#pragma once
+
+#include <vector>
+#include <string>
+
+#include ".\number.hpp"
+
+/*
+可以用 std::locale::global(std::locale("zh_CN.UTF-8")); 将本地环境设置为utf8编码、简体中文
+*/
+
+namespace OKps
+{
+    /*
+    c++20标准要求，char8_t与unsigned char具有相同的符号性、大小和对齐，
+    又要求一个值从char转换到unsigned char再转换回char，值不变，
+    故std::string和std::u8string之间的相互转换必然成立，且值不变。
+    */
+
+    /*
+    标准库定义的字符串类型
+    */
+    template<typename string_type>
+    concept stl_string_type = std::is_same_v<string_type, std::string>
+        or std::is_same_v<string_type, std::wstring>
+        or std::is_same_v<string_type, std::u8string>
+        or std::is_same_v<string_type, std::u16string>
+        or std::is_same_v<string_type, std::u32string>;
+
+    template<stl_string_type origin_type, stl_string_type target_type>
+    typename std::enable_if_t<sizeof(typename origin_type::value_type) == sizeof(typename target_type::value_type), target_type> value_cast(origin_type const & input);
+
+    template std::wstring value_cast<std::u16string, std::wstring>(std::u16string const &);
+    template std::u16string value_cast<std::wstring, std::u16string>(std::wstring const &);
+    template std::wstring value_cast<std::u32string, std::wstring>(std::u32string const &);
+    template std::u32string value_cast<std::wstring, std::u32string>(std::wstring const &);
+    template std::wstring value_cast<std::u8string, std::wstring>(std::u8string const &);
+    template std::u8string value_cast<std::wstring, std::u8string>(std::wstring const &);
+    template std::wstring value_cast<std::string, std::wstring>(std::string const &);
+    template std::string value_cast<std::wstring, std::string>(std::wstring const &);
+    template std::u8string value_cast<std::string, std::u8string>(std::string const &);
+    template std::string value_cast<std::u8string, std::string>(std::u8string const &);
+
+        /*
+        此函数仅用于向系统终端输出程序接受的命令行参数
+        在main函数开头将命令行参数转发给此函数
+
+        因为不同系统、不同终端模拟器可能对命令的格式规定和处理不同，用户实际输入给程序的命令行参数可能并不是他们所想的
+        */
+    void echo_command_line(int const argc, char const * const argv[]);
+
+    void wait_input(std::string const & hint = "输入回车以继续运行程序。在输入回车以前，输入任何字符都会被程序忽略。\n输入回车 ││ ", char const input = '\n');
+    /*
+    此函数将单个数字，如 0b1、0xa，转换成字符，如'1'、'a'
+    如果不能转换，则抛出异常
+    */
+    char to_char(unsigned int const number, number_system const number_base = number_system::dec);
+    /*
+    此函数将表示数字的字符，如 '1'、'a'，转换成整数类型，不区分大小写字母
+    如果不能转换，则抛出异常
+    c++标准规定，整数类型的内建算术运算符的操作数至少是int类型，char、short会被隐式转换到int再参与整数运算，故此函数返回int类型
+    */
+    unsigned int from_char(char const number, number_system const number_base = number_system::dec);
+    /*
+    命令语句分离器
+    输入任意字符串，将它转换为命令语句
+    也就是，头部和尾部没有空格，中间也不会出现连续多个空格
+    也就是说，任何字符串都会被转换成 "命令 参数1 参数2 参数3 ···"的形式
+    这里所说的空格，只是普通半角空格，不支持空字符、制表符、全角空格或其他特殊字符
+    */
+    class command_statement final
+    {
+    private:
+        /*
+        命令
+        */
+        std::string MEMBER_command;
+        /*
+        参数表
+        */
+        std::vector<std::string> MEMBER_argument_list;
+
+    public:
+        /*
+        命令，指输入的命令语句的第一个元素
+        */
+        const std::string & command() const noexcept;
+
+        /*
+        参数表
+        */
+        const std::vector<std::string> & argument_list() const noexcept;
+
+        /*
+        完整的命令语句字符串
+        */
+        std::string command_string() const;
+
+        command_statement(const std::string & origin_statement);
+
+        ~command_statement()
+            noexcept(std::is_nothrow_destructible_v<std::string>
+            and std::is_nothrow_destructible_v<std::vector<std::string>>);
+
+        command_statement(command_statement const & origin)
+            noexcept(std::is_nothrow_copy_constructible_v<std::string>
+            and std::is_nothrow_copy_constructible_v<std::vector<std::string>>);
+        command_statement(command_statement && origin)
+            noexcept(std::is_nothrow_move_constructible_v<std::string>
+            and std::is_nothrow_move_constructible_v<std::vector<std::string>>);
+        void operator =(command_statement const & origin)
+            noexcept(std::is_nothrow_copy_assignable_v<std::string>
+            and std::is_nothrow_copy_assignable_v<std::vector<std::string>>);
+        void operator =(command_statement && origin)
+            noexcept(std::is_nothrow_move_assignable_v<std::string>
+            and std::is_nothrow_move_assignable_v<std::vector<std::string>>);
+    };
+}
